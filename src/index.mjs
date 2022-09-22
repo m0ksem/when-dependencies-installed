3ef03a712fdf95a45a5c9454b253661d52fd5582
m0ksem <m0ksem1337@gmail.com>
2022-09-22 21:35:05 +0300

    Init app.

---
+++ src/index.mjs
@@ -0,0 +1,24 @@
+import { resolve, dirname } from 'path'
+import { readFileSync } from 'fs'
+import { execSync } from 'child_process'
+import fastGlob from 'fast-glob'
+
+const currentFilePath = dirname(import.meta.url.replace('file://', ''))
+const modulePath = resolve(currentFilePath, '..') // when-dependencies-installed module path
+const packagePath = resolve(modulePath, '..', '..') // existing from node_modules to current package
+
+// Resolving multiple package.json in monorepo
+const packageJsonPaths = fastGlob.sync(`${packagePath}/**/package.json`, {
+  ignore: ['**/node_modules/**'],
+})
+
+packageJsonPaths
+  .forEach((packageJsonPath) => {
+    const packageJson = JSON.parse(readFileSync(packageJsonPath, 'utf8'))
+
+    const script = packageJson.scripts?.['dependenciesInstalled'] || packageJson.scripts?.['dependencies-installed']
+
+    if (!script) { return }
+
+    execSync(script, { cwd: dirname(packageJsonPath), stdio: 'inherit' })
+  })
